72241c246d67946df707e95924c4b96a4252c3ae
Manfred <marodok@gmail.com>
2018-01-03 16:16:04 -0600

    Switch mode option

--- padma/library/visual-editor/scripts-src/switch-mode.js
+++ padma/library/visual-editor/scripts-src/switch-mode.js
@@ -21,6 +21,31 @@ define(['jquery', 'switch-mode'], function($) {
 
 	/**
 	 *
+	 * Read cookie
+	 *
+	 */	
+	readCookie = function(name) {
+		var nameEQ = name + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0;i < ca.length;i++) {
+		    var c = ca[i];
+		    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		}
+		return null;
+	}
+
+	/**
+	 *
+	 * Erease cookie
+	 *
+	 */	
+	eraseCookie = function(name) {
+		createCookie(name,"",-1);
+	}
+
+	/**
+	 *
 	 * Initialize
 	 *
 	 */

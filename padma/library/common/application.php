e788c5206d5c4953cd695da9300b73d418d40013
Manfred <marodok@gmail.com>
2018-01-29 14:45:11 -0600

    Fix

--- padma/library/common/application.php
+++ padma/library/common/application.php
@@ -415,12 +415,14 @@ class Padma {
 	 **/
 	public static function initiate_updater() {
 
+		/*
 		$GLOBALS['padma_updater'] = new Padma_Theme_Updater(array(
 			'remote_api_url' 	=> PADMA_API_URL,
 			'version' 			=> PADMA_VERSION,
 			'token' 			=> get_option('padma_service_token'),
 			'slug'				=> 'padma'
 		));
+		*/
 
 	}
 

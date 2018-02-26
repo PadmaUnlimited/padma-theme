08c5950eeefe2a4e2fd08e192f87d898dedcdcaf
Manfred <marodok@gmail.com>
2018-02-26 16:24:29 -0600

    Actualizado el Powered by

--- padma/library/blocks/footer/footer.php
+++ padma/library/blocks/footer/footer.php
@@ -135,8 +135,10 @@ class PadmaFooterBlock extends PadmaBlockAPI {
 	 **/
 	public static function show_padma_link() {
 		
+		
+
 		$padma_location = 'https://www.padmaunlimited.com/';
-		echo apply_filters('padma_link', '<p class="footer-left footer-padma-link footer-link">' . __('Powered by Padma Unlimited, the ', 'padma') . ' <a href="' . $padma_location . '" title="Padma Premium WordPress Theme">drag and drop WordPress theme</a></p>');
+		echo apply_filters('padma_link', '<p class="footer-left footer-padma-link footer-link">' . __('Powered by Padma Unlimited, the ', 'padma') . ' <a href="' . $padma_location . '" title="Powered by Padma Unlimited, the drag and drop WordPress theme builder">the drag and drop WordPress theme builder</a></p>');
 
 	}
 

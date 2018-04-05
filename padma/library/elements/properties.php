a20dfca849e98d44228d2ff71b9ab87e0e23855a
Manfred <marodok@gmail.com>
2018-04-05 11:55:06 -0600

    Animation test

--- padma/library/elements/properties.php
+++ padma/library/elements/properties.php
@@ -673,6 +673,121 @@ class PadmaElementProperties {
 				'unit' 	=> 'px',
 				'js-callback' => 'stylesheet.update_rule(params.selector, {"max-height": params.value + "px"});',
 			),
+
+		/*	Animation	*/
+			'width' => array(
+				'group' => 'Animation',
+				'name' 	=> 'Animation',
+				'type' 	=> 'select',
+				'options' => array(
+					// Attention Seekers
+					'bounce' 		=> 'bounce',
+					'flash' 		=> 'flash',
+					'pulse' 		=> 'pulse',
+					'rubberBand' 	=> 'rubberBand',
+					'shake' 		=> 'shake',
+					'swing' 		=> 'swing',
+					'tada' 			=> 'tada',
+					'wobble' 		=> 'wobble',
+					'jello' 		=> 'jello',
+
+					// Bouncing Entrances
+					'bounceIn' 		=> 'bounceIn',
+					'bounceInDown' 	=> 'bounceInDown',
+					'bounceInLeft' 	=> 'bounceInLeft',
+					'bounceInRight' => 'bounceInRight',
+					'bounceInUp' 	=> 'bounceInUp',
+
+					// Bouncing Exits
+					'bounceOut' 		=> 'bounceOut',
+					'bounceOutDown' 	=> 'bounceOutDown',
+					'bounceOutLeft' 	=> 'bounceOutLeft',
+					'bounceOutRight' 	=> 'bounceOutRight',
+					'bounceOutUp' 		=> 'bounceOutUp',
+
+					// Fading Entrances
+					'fadeIn' 			=> 'fadeIn',
+					'fadeInDown' 		=> 'fadeInDown',
+					'fadeInDownBig' 	=> 'fadeInDownBig',
+					'fadeInLeft' 		=> 'fadeInLeft',
+					'fadeInLeftBig' 	=> 'fadeInLeftBig',
+					'fadeInRight' 		=> 'fadeInRight',
+					'fadeInRightBig' 	=> 'fadeInRightBig',
+					'fadeInUp' 			=> 'fadeInUp',
+					'fadeInUpBig' 		=> 'fadeInUpBig',
+
+					// Fading Exits
+					'fadeOut' 			=> 'fadeOut',
+					'fadeOutDown' 		=> 'fadeOutDown',
+					'fadeOutDownBig' 	=> 'fadeOutDownBig',
+					'fadeOutLeft' 		=> 'fadeOutLeft',
+					'fadeOutLeftBig' 	=> 'fadeOutLeftBig',
+					'fadeOutRight' 		=> 'fadeOutRight',
+					'fadeOutRightBig' 	=> 'fadeOutRightBig',
+					'fadeOutUp' 		=> 'fadeOutUp',
+					'fadeOutUpBig' 		=> 'fadeOutUpBig',
+
+					// Flippers
+					'flip' 		=> 'flip',
+					'flipInX' 	=> 'flipInX',
+					'flipInY' 	=> 'flipInY',
+					'flipOutX' 	=> 'flipOutX',
+					'flipOutY' 	=> 'flipOutY',
+
+					// Lightspeed
+					'lightSpeedIn' 		=> 'lightSpeedIn',
+					'lightSpeedOut' 	=> 'lightSpeedOut',
+
+					// Rotating Entrances
+					'rotateIn' 			=> 'rotateIn',
+					'rotateInDownLeft' 	=> 'rotateInDownLeft',
+					'rotateInDownRight' => 'rotateInDownRight',
+					'rotateInUpLeft' 	=> 'rotateInUpLeft',
+					'rotateInUpRight' 	=> 'rotateInUpRight',
+
+					// Rotating Exits
+					'rotateOut' 			=> 'rotateOut',
+					'rotateOutDownLeft' 	=> 'rotateOutDownLeft',
+					'rotateOutDownRight' 	=> 'rotateOutDownRight',
+					'rotateOutUpLeft' 		=> 'rotateOutUpLeft',
+					'rotateOutUpRight' 		=> 'rotateOutUpRight',
+
+					// Sliding Entrances
+					'slideInUp' 	=> 'slideInUp',
+					'slideInDown' 	=> 'slideInDown',
+					'slideInLeft' 	=> 'slideInLeft',
+					'slideInRight' 	=> 'slideInRight',
+
+					// Sliding Exits
+					'slideOutUp' 	=> 'slideOutUp',
+					'slideOutDown' 	=> 'slideOutDown',
+					'slideOutLeft' 	=> 'slideOutLeft',
+					'slideOutRight' => 'slideOutRight',
+
+
+					// Zoom Entrances
+					'zoomIn' 		=> 'zoomIn',
+					'zoomInDown' 	=> 'zoomInDown',
+					'zoomInLeft' 	=> 'zoomInLeft',
+					'zoomInRight' 	=> 'zoomInRight',
+					'zoomInUp' 		=> 'zoomInUp',
+
+					// Zoom Exits
+					'zoomOut' 		=> 'zoomOut',
+					'zoomOutDown' 	=> 'zoomOutDown',
+					'zoomOutLeft' 	=> 'zoomOutLeft',
+					'zoomOutRight' 	=> 'zoomOutRight',
+					'zoomOutUp' 	=> 'zoomOutUp',
+
+					// Specials
+					'hinge' 		=> 'hinge',
+					'jackInTheBox' 	=> 'jackInTheBox',
+					'rollIn' 		=> 'rollIn',
+					'rollOut' 		=> 'rollOut',
+
+				),
+				'js-callback' => 'stylesheet.update_rule(params.selector, {"width": params.value + "px"});',
+			),
 	);
 	
 	

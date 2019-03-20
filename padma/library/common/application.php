1fc99077f36ad5a588eaa6f116a999f4224d3995
Manfred <marodok@gmail.com>
2019-03-20 11:15:54 -0600

    Updated version

--- padma/library/common/application.php
+++ padma/library/common/application.php
@@ -32,7 +32,7 @@ class Padma {
 			define('THEME_FRAMEWORK', 'padma');			
 		}
 		if(!defined('PADMA_VERSION')){
-			define('PADMA_VERSION', '1.0.0');			
+			define('PADMA_VERSION', '1.0.1');			
 		}
 
 		/* Define directories */

8fdceb9593ba39f4d7c53aeadc4d95011049ef27
dosunos <dosunos@gmail.com>
2019-03-29 10:01:05 -0600

    WooCommerce product button fix.

--- padma/library/compatibility/woocommerce/woocommerce-design-elements.php
+++ padma/library/compatibility/woocommerce/woocommerce-design-elements.php
@@ -133,7 +133,17 @@ function padma_storefront_wc_register_elements() {
 		'description' => 'Storefront: WooCommerce',
 		'selector' => '.woocommerce .button a.product_type_simple.add_to_cart_button.ajax_add_to_cart'
 	));
-
+	
+	PadmaElementAPI::register_element(array(
+		'group' => 'blocks',
+		'id' => 'block-content-wc-product-page-button-item',
+		'parent' => 'block-content',
+		'name' => 'Product Page: Button',
+		'indent-in-selector' => true,
+		'description' => 'Storefront: WooCommerce Button',
+		'selector' => '.woocommerce ul.products li.product .button'
+	));
+	
 	PadmaElementAPI::register_element(array(
 		'group' => 'blocks',
 		'id' => 'block-content-wc-product-page-button-text',

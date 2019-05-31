3d8249a8215009a75f1ad460ab7c578b2a91c44c
Manfred <marodok@gmail.com>
2019-05-31 16:12:30 -0600

    Plugin templates fix

--- padma/library/common/application.php
+++ padma/library/common/application.php
@@ -32,7 +32,7 @@ class Padma {
 			define('THEME_FRAMEWORK', 'padma');			
 		}
 		if(!defined('PADMA_VERSION')){
-			define('PADMA_VERSION', '1.1.5');			
+			define('PADMA_VERSION', '1.1.6');			
 		}
 
 		/* Define directories */

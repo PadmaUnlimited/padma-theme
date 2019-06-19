ce6447280c1eb3a3d08884b5c1ebbaaec77ce62b
Manfred <marodok@gmail.com>
2019-06-19 12:25:55 -0600

    Display Padma Blocks in Gutenberg

--- padma/library/common/application.php
+++ padma/library/common/application.php
@@ -32,7 +32,7 @@ class Padma {
 			define('THEME_FRAMEWORK', 'padma');			
 		}
 		if(!defined('PADMA_VERSION')){
-			define('PADMA_VERSION', '1.1.11');			
+			define('PADMA_VERSION', '1.1.12');			
 		}
 
 		/* Define directories */

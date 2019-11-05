28180aee0aa080d1c0fc7292716589875997b655
Manfred <marodok@gmail.com>
2019-11-05 10:11:42 -0600

    Mobile VE fixes

--- padma/library/common/application.php
+++ padma/library/common/application.php
@@ -32,7 +32,7 @@ class Padma {
 			define('THEME_FRAMEWORK', 'padma');			
 		}
 		if(!defined('PADMA_VERSION')){
-			define('PADMA_VERSION', '1.1.60');			
+			define('PADMA_VERSION', '1.1.61');			
 		}
 
 		/* Define directories */

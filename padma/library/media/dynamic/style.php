7dd8a443672c190714322519425eb1fd5b183d4c
Manfred <marodok@gmail.com>
2020-01-20 17:20:57 -0600

    Minor fix

--- padma/library/media/dynamic/style.php
+++ padma/library/media/dynamic/style.php
@@ -215,8 +215,6 @@ class PadmaDynamicStyle {
 				$wrapper_selector = 'div#wrapper-' . PadmaWrappers::format_wrapper_id($wrapper_id);
 			}
 
-			//debug($wrapper_settings);
-
 			/* Fixed Wrapper */
 				if ( !padma_get('fluid', $wrapper_settings, false, true) ) {
 

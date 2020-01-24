445bc0fbb06147f8b7b9cf0e7ad724ad166c436d
Manfred <marodok@gmail.com>
2020-01-24 16:26:24 -0600

    Updated Texts

--- padma/library/admin/pages/getting-started.php
+++ padma/library/admin/pages/getting-started.php
@@ -21,29 +21,47 @@ defined('ABSPATH') or die("No script kiddies please!");
 
 	<div id="welcome" class="padma-admin-tab" style="">	
 		<div class="content">
+
 			<h1><?php _e('Welcome!','padma'); ?></h1>
+
 			<p><?php _e('Your <strong>Padma Theme Builder</strong> installation is ready.','padma'); ?></p>
-			<p><strong><?php _e('You can start creating now!','padma'); ?></strong></p>
+			
+			<p><strong><?php _e('Start creating now!','padma'); ?></strong></p>
+
 			<br>
-			<p><?php _e('If you want to hide this page just change the Default Admin Page in <a href="?page=padma-options">Padma » Options</a>.','padma'); ?></p>
+
+			<p><?php _e('To hide this page just change the Default Admin Page in <a href="?page=padma-options">Padma » Options</a>.','padma'); ?></p>
+
 			<div class="separator"></div>
-			<h2><?php _e('Padma | unlimited - Core features in the WordPress dashboard / admin menu.','padma'); ?></h2>
+
+			<h2><?php _e('Padma | Unlimited - Core features in the WordPress dashboard / admin menu.','padma'); ?></h2>
 			<div class="box">
-				<h3><?php _e('Padma | unlimited - Welcome!','padma'); ?></h3>
+				<h3><?php _e('Padma | Unlimited - Welcome!','padma'); ?></h3>
 				<p><?php _e('(This page)','padma'); ?></p>
-				<p><?php _e('Get access to General Information, Documentation & Support for Padma | unlimited - WordPress Theme Builder.','padma'); ?></p>
-				<p><?php _e('Get access to blocks and templates to improve your Padma | unlimited Theme Builder.','padma'); ?></p>
-				<br>
-				<p><?php _e('If you are new to Padma | unlimited, please read our <a href="https://docs.padmaunlimited.com/">Free WordPress Theme Builder Documentation</a>','padma'); ?></p>
+				<p><?php _e('Get access to ►','padma'); ?></p>
+				<p><?php _e('General Information, Documentation and Support for <b>Padma</b> | Unlimited Theme Builder.','padma'); ?></p>
+				<p><?php _e('Blocks and templates to expand <b>Padma</b> | Unlimited Theme Builder possibilities.','padma'); ?></p>
 			</div>
+			
+
+			<h2><?php _e('Padma starter users','padma'); ?></h2>
 			<div class="box">
-				<h3><?php _e('Padma | unlimited Editor','padma'); ?></h3>
-				<p><?php _e('Padma | unlimited Editor is a powerful tool to design & customize your layouts and edit the visual style of your WordPress website through a graphical interface.','padma'); ?></p>
-				<p><?php _e('Learn more abou it in the document <a rel="noopener" href="https://docs.padmaunlimited.com/blog/basics/before-using-the-visual-editor/">Introduction to the Padma | unlimited Editor.</a>','padma'); ?></p>
-				<a href="<?php echo home_url() . '/?visual-editor=true'; ?>" class="access-to-unlimited-editor"><span class="text"><?php _e('Access Padma | Unlimited Editor','padma'); ?></span><span class="line -right"></span><span class="line -top"></span><span class="line -left"></span><span class="line -bottom"></span></a>
+				<p><?php _e('Please read Padma | Unlimited Theme Builder <a href="https://docs.padmaunlimited.com/">Documentation</a>.','padma'); ?></p>
+			</div>
+
+
+			<div class="box">
+				<h3><?php _e('Padma | Unlimited Visual Editor','padma'); ?></h3>
+				
+				<p><?php _e('Padma | Unlimited Visual Editor is a powerful tool to design WordPress website layouts and templates.  Easily customize almost every visual element of your websites through a graphical interface (Code can be added easily using the integrated code editor if required).','padma'); ?></p>
+
+				<p><?php _e('Learn more about the platform in the document <a rel="noopener" href="https://docs.padmaunlimited.com/blog/basics/before-using-the-visual-editor/">"Introduction to Padma | Unlimited Visual Editor". </a>','padma'); ?></p>
+
+				<a href="<?php echo home_url() . '/?visual-editor=true'; ?>" class="access-to-unlimited-editor"><span class="text"><?php _e('Access <b>Padma</b> | Unlimited Editor','padma'); ?></span><span class="line -right"></span><span class="line -top"></span><span class="line -left"></span><span class="line -bottom"></span></a>
 			</div>
 			<div class="box">
 				<h3><?php _e('Additional Blocks available!','padma'); ?></h3>
+				<p><?php _e('Padma dashboard allows users to manage their Padma | Unlimited installations. Also, Discover Padma tiers subscription to access additional Plugins and Services.','padma'); ?></p>
 				<a href="https://dashboard.padmaunlimited.com/login"><?php _e('Register to Padma Services to get access to additional Plugins and Blocks','padma'); ?></a>				
 			</div>
 		</div>
@@ -58,36 +76,36 @@ defined('ABSPATH') or die("No script kiddies please!");
 			<h2 class="center"><?php _e('Padma | Tools','padma'); ?></h2>
 			<div class="box">
 				<h3><?php _e('System info.','padma'); ?></h3>
-				<p><?php _e('You should provide this system information If you want to open a ticket or if you log a help request on the forums','padma'); ?></p>
+				<p><?php _e('To open a ticket or if you log a help request on the forums, please provide this system information.','padma'); ?></p>
 			</div>
 			<div class="box">
 				<h3><?php _e('Snapshots','padma'); ?></h3>
-				<p><?php _e('Here you can delete the snapshots of Padma Theme Builder to free up some disk space.','padma'); ?></p>
+				<p><?php _e('To free up some disk space, please delete Padma | Theme Builder snapshots.','padma'); ?></p>
 			</div>
 			<div class="box">
 				<h3><?php _e('Reset','padma'); ?></h3>
-				<p><?php _e('Instructions to reset your <strong>Padma | Unlimited</strong> WordPress Theme Builder installation.','padma'); ?></p>
+				<p><?php _e('Instructions to reset your Padma | Unlimited WordPress Theme Builder installation.','padma'); ?></p>
 			</div>
 		</div>
 	</div>
 
 	<div id="need-help" class="padma-admin-tab" style="display:none">
 		<div class="content">
-			<h2 class="center"><?php _e('Need help?','padma'); ?></h2>
-			<p><strong><?php _e('Padma | unlimited WordPress Theme Builder</strong> provides professional support and comprehensive documentation to help you bring your projects alive.','padma'); ?></p>			
+			<h2 class="center"><?php _e('Help','padma'); ?></h2>
+			<p><?php _e('Padma | Unlimited Theme Builder provides professional support and comprehensive documentation to help you bring your projects alive.','padma'); ?></p>			
 			<div class="separator"></div>
 			<div class="box">
 				<h3><?php _e('Padma | Community','padma'); ?></h3>
-				<p><?php _e('Join to our great community, we help us each other across different channels.','padma'); ?></p>
+				<p><?php _e('Join our community, get involved and help each other across multiple channels.','padma'); ?></p>
 				<p><a href="https://www.facebook.com/padmaunlimited/" target="_blank"><?php _e('Facebook Page','padma'); ?></a></p>
 				<p><a href="https://www.facebook.com/groups/367999217036886/" target="_blank"><?php _e('Facebook Users Group (English)','padma'); ?></a></p>
 				<p><a href="https://www.facebook.com/groups/291445981578459/" target="_blank"><?php _e('Facebook Users Group (Español)','padma'); ?></a></p>
 				<p><a href="https://twitter.com/PadmaUnlimited" target="_blank">Twitter</a></p>
-				<p><a href="https://join.slack.com/t/padma-unlimited/shared_invite/enQtNTAxMzM1NjcwNTc5LTM2YzQ0ODRhYzBmZDc4N2UwOWE0MjBlMmQyZmQ2MTdjZTgyNjg1Mzk4ZjVlNGIxYjZkMjlmMTNhNmE3OWQ1YjY" target="_blank">Slack</a></p>
+				<p><a href="https://join.slack.com/t/padma-unlimited/shared_invite/enQtNTAxMzM1NjcwNTc5LWVmZjliNDRhZTQ1Y2FhZDY3ZjdkNzMzYzRmMzEwMDEyMWY0MjllYzJhYTk4ZTMxODEzNjk5NzE1YzMyMjFjNmY" target="_blank">Slack</a></p>
 			</div>
 			<div class="box">
 				<h3><?php _e('Padma | Documentation','padma'); ?></h3>
-				<p><?php _e('Register with us and get <strong>free access</strong> to our complete and comprehensive documentation. <a target="_blank" href="https://docs.padmaunlimited.com/" rel="noopener">docs.padmaunlimited.com</a>','padma'); ?></p>
+				<p><?php _e('Register with us and get free access to our in- depth documentation. <a target="_blank" href="https://docs.padmaunlimited.com/" rel="noopener">docs.padmaunlimited.com</a>','padma'); ?></p>
 			</div>
 		</div>
 	</div>
@@ -95,17 +113,19 @@ defined('ABSPATH') or die("No script kiddies please!");
 	<div id="unlimited-growth" class="padma-admin-tab" style="display:none">
 		<div class="content">
 			<h2 class="center"><?php _e('Padma | Unlimited Growth','padma'); ?></h2>			
-			<p><?php _e('Develop, share and sale Templates & Custom Blocks. Working together will increase community growth possibilities. Design faster & Customize your Workflow with useful tools. Don´t hesitate to invest in other strategic areas to your business.','padma'); ?></p>
+			<p><?php _e('Develop, share and put on the market Templates and Custom Blocks.','padma'); ?></p>
+			<p><?php _e('Working together will increase the community growth so, get involved!','padma'); ?></p>
+			<p><?php _e('Design faster and Customize your Workflow with useful tools.','padma'); ?></p>
 			<div class="separator"></div>
 			<div class="box">
 				<h3><?php _e('Padma | Unlimited Templates','padma'); ?></h3>
-				<p><?php _e('<strong>Padma | unlimited Templates</strong> minimize your project development process, streamlining the design stage leading a faster content creation and upload.','padma'); ?></p>
+				<p><?php _e('<strong>Padma | Unlimited Templates</strong> minimize your project development process, streamlining the design stage leading a faster content creation and upload.','padma'); ?></p>
 			</div>
 			<div class="box">
 				<h3><?php _e('Padma | Unlimited Blocks','padma'); ?></h3>
-				<p><?php _e('Extend the functionality of your Padma | unlimited WordPress Theme Builder installation by adding useful custom blocks to your Projects.','padma'); ?></p>
-				<p><?php _e('Services: Documentation, Lifesaver (Migrate from HTW/Blox), Child Theme, Templates on cloud, site monitor.','padma'); ?></p>				
-				<p><?php _e('Shortcode block: integration & management of Woocommerce content. Contact form 7 & Gravity forms.','padma'); ?></p>
+				<p><?php _e('Extend the functionality of your Padma | Unlimited WordPress Theme Builder installation by adding useful custom blocks to your Projects.','padma'); ?></p>
+				<p><?php _e('Services: Documentation, Lifesaver (Migrate from HTW/Blox), Child Theme, Templates on cloud, site monitor.','padma'); ?></p>								
+				<a href="https://dashboard.padmaunlimited.com/login"><?php _e('Discover available Plugins and Blocks.','padma'); ?></a>	
 				<p><?php _e('Get into Padma Services to unlock the Padma Unlimited potential.','padma'); ?></p>
 			</div>
 		</div>
--- padma/library/common/application.php
+++ padma/library/common/application.php
@@ -32,7 +32,7 @@ class Padma {
 			define('THEME_FRAMEWORK', 'padma');			
 		}
 		if(!defined('PADMA_VERSION')){
-			define('PADMA_VERSION', '1.1.98');
+			define('PADMA_VERSION', '1.1.99');
 		}
 
 		/* Define directories */

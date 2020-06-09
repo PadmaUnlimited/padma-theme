3d08b0b10a9f4aafe76a71c40afa04ee95175533
Manfred <marodok@gmail.com>
2020-06-08 18:22:07 -0600

    Changes for 1.2.20 version

--- padma/library/visual-editor/scripts-src/modules/grid/wrappers.js
+++ padma/library/visual-editor/scripts-src/modules/grid/wrappers.js
@@ -446,8 +446,10 @@ define(['util.custommouse', 'qtip', 'helper.data', 'modules/grid/wrapper-inputs'
 
 
 					var orgWrapper = wrapper.clone();
+					/*
 					console.log(wrapper);
 					console.log(orgWrapper);
+					*/
 
 					if(typeof isSavingAllowed == 'undefined'){
 						isSavingAllowed = false;

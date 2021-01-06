c6238586744e3edab09a5a128ca788404f9d6a9b
Manfred <marodok@gmail.com>
2021-01-06 10:21:47 -0600

    Coding standard updates

--- padma/library/visual-editor/visual-editor.php
+++ padma/library/visual-editor/visual-editor.php
@@ -1,31 +1,60 @@
 <?php
+/**
+ * Padma Theme Visual Editor
+ *
+ * @since 1.0.0
+ * @package Padma
+ */
 
 /**
  * Visual Editor Main Class
  */
 class PadmaVisualEditor {
 
+	/**
+	 * Visual Editor Modes
+	 *
+	 * @var array
+	 */
+	protected static $modes = array();
 
-	protected static $modes 			= array();	
-	protected static $default_mode 		= 'grid';
-	protected static $default_layout 	= 'index';
+	/**
+	 * Visual Editor Default Mode
+	 *
+	 * @var string
+	 */
+	protected static $default_mode = 'grid';
+
+	/**
+	 * Default layout
+	 *
+	 * @var string
+	 */
+	protected static $default_layout = 'index';
 
 
+	/**
+	 * Init method.
+	 *
+	 * @return void
+	 */
 	public static function init() {
 
 		if ( ! PadmaCapabilities::can_user_visually_edit() ) {
 			return;
 		}
 
-		//If no child theme is active or if a child theme IS active and the grid is supported, use the grid mode.
-		if ( current_theme_supports('padma-grid') )
-			self::$modes['Grid'] = __('Add blocks and arrange your website structure','padma');
+		// If no child theme is active or if a child theme IS active and the grid is supported, use the grid mode.
+		if ( current_theme_supports( 'padma-grid' ) ) {
+			self::$modes['Grid'] = __( 'Add blocks and arrange your website structure', 'padma' );
+		}
 
-		self::$modes['Design'] = __('Choose fonts, colors, and other styles','padma');
+		self::$modes['Design'] = __( 'Choose fonts, colors, and other styles', 'padma' );
 
-		//If the grid is disabled, set Design as the default mode.
-		if ( !current_theme_supports('padma-grid') )
+		// If the grid is disabled, set Design as the default mode.
+		if ( ! current_theme_supports( 'padma-grid' ) ) {
 			self::$default_mode = 'design';
+		}
 
 		PadmaSettings::set_visual_editor_settings();
 
@@ -33,13 +62,13 @@ class PadmaVisualEditor {
 		do_action( 'padma_visual_editor_init' );
 
 		// Visual Editor AJAX.
-		add_action('wp_ajax_padma_visual_editor', array(__CLASS__, 'ajax'));
+		add_action( 'wp_ajax_padma_visual_editor', array( __CLASS__, 'ajax' ) );
 
-		if ( PadmaOption::get('debug-mode') ) {
-			add_action('wp_ajax_nopriv_padma_visual_editor', array(__CLASS__, 'ajax'));
+		if ( PadmaOption::get( 'debug-mode' ) ) {
+			add_action( 'wp_ajax_nopriv_padma_visual_editor', array( __CLASS__, 'ajax' ) );
 		}
 
-		//Cache rejection
+		// Cache rejection.
 		global $cache_rejected_uri;
 
 		if ( ! is_array( $cache_rejected_uri ) ) {
@@ -50,24 +79,26 @@ class PadmaVisualEditor {
 		$cache_rejected_uri[] = 've\-iframe\=true';
 
 		// iframe handling.
-		add_action('padma_body_close', array(__CLASS__, 'iframe_load_flag'));
-		add_action('padma_grid_iframe_footer', array(__CLASS__, 'iframe_load_flag'));
+		add_action( 'padma_body_close', array( __CLASS__, 'iframe_load_flag' ) );
+		add_action( 'padma_grid_iframe_footer', array( __CLASS__, 'iframe_load_flag' ) );
 
-		add_action('padma_grid_iframe_footer', array(__CLASS__, 'iframe_tooltip_container'));
-		add_action('padma_body_close', array(__CLASS__, 'iframe_tooltip_container'));
+		add_action( 'padma_grid_iframe_footer', array( __CLASS__, 'iframe_tooltip_container' ) );
+		add_action( 'padma_body_close', array( __CLASS__, 'iframe_tooltip_container' ) );
 
-        wp_enqueue_media();
+		wp_enqueue_media();
 
-		padma_register_web_font_provider('PadmaTraditionalFonts');
+		padma_register_web_font_provider( 'PadmaTraditionalFonts' );
 
-		if( ! PadmaOption::get('do-not-use-google-fonts') ){
-			padma_register_web_font_provider('PadmaGoogleFonts');
-		}			
-
-
+		if ( ! PadmaOption::get( 'do-not-use-google-fonts' ) ) {
+			padma_register_web_font_provider( 'PadmaGoogleFonts' );
+		}
 	}
 
-
+	/**
+	 * Ajax control method
+	 *
+	 * @return void
+	 */
 	public static function ajax() {
 
 		if ( ! defined( 'DONOTCACHEDB' ) ) {
@@ -78,46 +109,56 @@ class PadmaVisualEditor {
 			define( 'DONOTCACHCEOBJECT', true );
 		}
 
-		Padma::load('visual-editor/display', 'VisualEditorDisplay');
-		Padma::load('visual-editor/visual-editor-ajax');
+		Padma::load( 'visual-editor/display', 'VisualEditorDisplay' );
+		Padma::load( 'visual-editor/visual-editor-ajax' );
 
-		//Authenticate nonce
-		check_ajax_referer('padma-visual-editor-ajax', 'security');
+		// Authenticate nonce.
+		check_ajax_referer( 'padma-visual-editor-ajax', 'security' );
 
-		$method = padma_post('method') ? padma_post('method') : padma_get('method');
+		$method = padma_post( 'method' ) ? padma_post( 'method' ) : padma_get( 'method' );
 
-		//Check for a non-secure (something that doesn't save data) AJAX request first (let debug mode authentication pass through)
-		if ( method_exists('PadmaVisualEditorAJAX', 'method_' . $method) && PadmaCapabilities::can_user_visually_edit() ) {
-			do_action('padma_visual_editor_ajax_pre_' . $method);
-			call_user_func(array('PadmaVisualEditorAJAX', 'method_' . $method));
-			do_action('padma_visual_editor_ajax_post_' . $method);
-		}
+		// Check for a non-secure (something that doesn't save data) AJAX request first (let debug mode authentication pass through).
+		if ( method_exists( 'PadmaVisualEditorAJAX', 'method_' . $method ) && PadmaCapabilities::can_user_visually_edit() ) {
+			do_action( 'padma_visual_editor_ajax_pre_' . $method );
+			call_user_func( array( 'PadmaVisualEditorAJAX', 'method_' . $method ) );
+			do_action( 'padma_visual_editor_ajax_post_' . $method );
 
-		//Check for a secure (something that saves data) AJAX request and require genuine authentication
-		elseif ( method_exists('PadmaVisualEditorAJAX', 'secure_method_' . $method) && PadmaCapabilities::can_user_visually_edit(true) ) {
-			do_action('padma_visual_editor_ajax_pre_' . $method);
-			call_user_func(array('PadmaVisualEditorAJAX', 'secure_method_' . $method));
-			do_action('padma_visual_editor_ajax_post_' . $method);
+			// Check for a secure (something that saves data) AJAX request and require genuine authentication.
+		} elseif ( method_exists( 'PadmaVisualEditorAJAX', 'secure_method_' . $method ) && PadmaCapabilities::can_user_visually_edit( true ) ) {
+			do_action( 'padma_visual_editor_ajax_pre_' . $method );
+			call_user_func( array( 'PadmaVisualEditorAJAX', 'secure_method_' . $method ) );
+			do_action( 'padma_visual_editor_ajax_post_' . $method );
 		}
 
 		die();
-
 	}
 
-	
-	public static function ajax_error_handler($errno, $errstr, $errfile, $errline) {
+	/**
+	 * Ajax error handler method
+	 *
+	 * @param int    $errno Error number.
+	 * @param string $errstr Error string.
+	 * @param string $errfile Error file.
+	 * @param int    $errline Error line.
+	 * @return void
+	 */
+	public static function ajax_error_handler( $errno, $errstr, $errfile, $errline ) {
 
-		if ( !defined( 'E_STRICT' ) )
+		if ( ! defined( 'E_STRICT' ) ) {
 			define( 'E_STRICT', 2048 );
+		}
 
-		if ( !defined( 'E_RECOVERABLE_ERROR' ) )
+		if ( ! defined( 'E_RECOVERABLE_ERROR' ) ) {
 			define( 'E_RECOVERABLE_ERROR', 4096 );
+		}
 
-		if ( !defined( 'E_DEPRECATED' ) )
+		if ( ! defined( 'E_DEPRECATED' ) ) {
 			define( 'E_DEPRECATED', 8192 );
+		}
 
-		if ( !defined( 'E_USER_DEPRECATED' ) )
+		if ( ! defined( 'E_USER_DEPRECATED' ) ) {
 			define( 'E_USER_DEPRECATED', 16384 );
+		}
 
 		$severity =
 			1 * E_ERROR |
@@ -138,7 +179,7 @@ class PadmaVisualEditor {
 
 		$error_ex = new ErrorException( $errstr, 0, $errno, $errfile, $errline );
 
-		if ( ( $error_ex->getSeverity() & $severity ) != 0 ) {
+		if ( ( $error_ex->getSeverity() & $severity ) !== 0 ) {
 			throw $error_ex;
 		}
 
@@ -187,8 +228,8 @@ class PadmaVisualEditor {
 									continue 2;
 
 								$args = array(
-									'position' => padma_get('position', $wrappers[$id], 9999),
-									'settings' => padma_get('settings', $wrappers[$id], array())
+									'position' => padma_get( 'position', $wrappers[$id], 9999),
+									'settings' => padma_get( 'settings', $wrappers[$id], array())
 								);
 
 								if ( $wrappers[$id]['insert_id'] ) {
@@ -198,7 +239,7 @@ class PadmaVisualEditor {
 								$new_wrapper = PadmaWrappersData::add_wrapper($current_layout, $args);
 
 								if ( is_wp_error($new_wrapper) ) {
-									$output['errors'][] = $new_wrapper->get_error_code() . ($new_wrapper->get_error_message() ? ' - ' . $new_wrapper->get_error_code() : '');
+									$output['errors'][] = $new_wrapper->get_error_code() . ($new_wrapper->get_error_message() ? ' - ' . $new_wrapper->get_error_code() : '' );
 								} else {
 									$output['wrapper-id-mapping'][$id] = $new_wrapper;
 								}
@@ -232,8 +273,8 @@ class PadmaVisualEditor {
 								if ( isset($blocks[$id]['delete']) )
 									continue 2;
 
-								$dimensions = explode(',', $blocks[$id]['dimensions']);
-								$position = explode(',', $blocks[$id]['position']);
+								$dimensions = explode( ',', $blocks[$id]['dimensions']);
+								$position = explode( ',', $blocks[$id]['position']);
 
 								$settings = isset($blocks[$id]['settings']) ? $blocks[$id]['settings'] : array();
 
@@ -243,12 +284,12 @@ class PadmaVisualEditor {
 								}
 
 								/* If 'duplicateOf' is present in the $settings array then remove that key and pull in the options from the block that's being duplicated */
-								$duplicate = padma_get('duplicateOf', $settings);
+								$duplicate = padma_get( 'duplicateOf', $settings);
 
 								if ( $duplicate ) {
 
 									$duplicated_block = PadmaBlocksData::get_block($duplicate);
-									$settings = padma_array_merge_recursive_simple(padma_get('settings', $duplicated_block), $settings);
+									$settings = padma_array_merge_recursive_simple(padma_get( 'settings', $duplicated_block), $settings);
 
 									unset($settings['duplicateOf']);
 
@@ -275,7 +316,7 @@ class PadmaVisualEditor {
 								$new_block = PadmaBlocksData::add_block($current_layout, $args);
 
 								if ( is_wp_error($new_block) ) {
-									$output['errors'][] = $new_block->get_error_code() . ($new_block->get_error_message() ? ' - ' . $new_block->get_error_code() : '');
+									$output['errors'][] = $new_block->get_error_code() . ($new_block->get_error_message() ? ' - ' . $new_block->get_error_code() : '' );
 								} else {
 
 									/* Add styling for duplicate if necessary */
@@ -286,9 +327,9 @@ class PadmaVisualEditor {
 										/* Go through and process styling */
 										foreach ( $duplicated_block_styling as $instance_id => $instance ) {
 
-											foreach ( padma_get('properties', $instance, array()) as $property => $property_value ) {
+											foreach ( padma_get( 'properties', $instance, array()) as $property => $property_value ) {
 
-												$instance_id = str_replace('block-' . $duplicated_block['id'], 'block-' . $new_block, $instance_id);
+												$instance_id = str_replace( 'block-' . $duplicated_block['id'], 'block-' . $new_block, $instance_id);
 
 												PadmaElementsData::set_special_element_property(null, $instance['element'], 'instance', $instance_id, $property, $property_value);
 
@@ -318,7 +359,7 @@ class PadmaVisualEditor {
 								if ( isset($blocks[$id]['new']) )
 									continue 2;
 
-								$dimensions = explode(',', $value);
+								$dimensions = explode( ',', $value);
 
 								$args = array(
 									'dimensions' => array(
@@ -336,7 +377,7 @@ class PadmaVisualEditor {
 								if ( isset($blocks[$id]['new']) )
 									continue 2;
 
-								$position = explode(',', $value);
+								$position = explode( ',', $value);
 
 								$args = array(
 									'position' => array(
@@ -376,7 +417,7 @@ class PadmaVisualEditor {
 								$block = PadmaBlocksData::get_block($id);
 
 								// Get block settings
-								$settings = padma_get('settings', $block);
+								$settings = padma_get( 'settings', $block);
 
 								if(!is_array($settings))
 									$settings = $blocks[$id]['settings'];
@@ -530,7 +571,7 @@ class PadmaVisualEditor {
 								 	//CSS transform support
 									case 'skew':
 										$property_id 	= 'transform';
-										$property_value = 'skew('.$property_value.'deg)';
+										$property_value = 'skew( '.$property_value.'deg)';
 										break;
 
 								 	//Margin Top Auto
@@ -563,9 +604,9 @@ class PadmaVisualEditor {
 							}
 
 							//Handle instances, states, etc.
-						} else if ( strpos($element_data_node, 'special-element-') === 0 ) {
+						} else if ( strpos($element_data_node, 'special-element-' ) === 0 ) {
 
-							$special_element_type = str_replace('special-element-', '', $element_data_node);
+							$special_element_type = str_replace( 'special-element-', '', $element_data_node);
 
 							//Loop through the special elements							
 							foreach ( $element_data_node_data as $special_element => $special_element_properties ) {
@@ -574,7 +615,7 @@ class PadmaVisualEditor {
 								if ( isset($output['block-id-mapping']) && count($output['block-id-mapping']) ) {
 
 									foreach ( $output['block-id-mapping'] as $old_block_id => $new_block_id ) {
-										$special_element = str_replace('block-' . $old_block_id, 'block-' . $new_block_id, $special_element);
+										$special_element = str_replace( 'block-' . $old_block_id, 'block-' . $new_block_id, $special_element);
 									}
 
 								}
@@ -601,7 +642,7 @@ class PadmaVisualEditor {
 									 	//CSS transform support
 										case 'skew':
 											$special_element_property 		= 'transform';
-											$special_element_property_value = 'skew('.$special_element_property_value.'deg)';
+											$special_element_property_value = 'skew( '.$special_element_property_value.'deg)';
 											break;
 
 									 	//Margin Top Auto
@@ -636,7 +677,7 @@ class PadmaVisualEditor {
 									);
 
 
-									if ( padma_get('js-property', $design_editor_properties[$special_element_property] ) ) {
+									if ( padma_get( 'js-property', $design_editor_properties[$special_element_property] ) ) {
 										PadmaElementsData::set_js_property($element_id . '||' . $special_element_type . '||' . $special_element, $special_element_property, $special_element_property_value);
 									}
 								}
@@ -658,21 +699,21 @@ class PadmaVisualEditor {
 			Padma::set_autoload();
 
 			//This hook is used by cache flushing, plugins, etc.  Do not fire on preview save because it'll flush preview options
-			if ( !padma_get('ve-preview') ) {
+			if ( !padma_get( 've-preview' ) ) {
 				
-				do_action('padma_visual_editor_save');
+				do_action( 'padma_visual_editor_save' );
 
-				if (PadmaOption::get('headway-support')) {
-					do_action('headway_visual_editor_save');
+				if (PadmaOption::get( 'headway-support' ) ) {
+					do_action( 'headway_visual_editor_save' );
 				}
 
-				if (PadmaOption::get('bloxtheme-support')) {
-					do_action('blox_visual_editor_save');
+				if (PadmaOption::get( 'bloxtheme-support' ) ) {
+					do_action( 'blox_visual_editor_save' );
 				}
 			}
 
 			/* Save snapshot if allowed */
-			if ( !defined('PADMA_DISABLE_AUTO_SNAPSHOT') || PADMA_DISABLE_AUTO_SNAPSHOT !== true ) {
+			if ( ! defined( 'PADMA_DISABLE_AUTO_SNAPSHOT' ) || PADMA_DISABLE_AUTO_SNAPSHOT !== true ) {
 				$output['snapshot'] = PadmaDataSnapshots::save_snapshot(true);
 			}
 
@@ -701,7 +742,7 @@ class PadmaVisualEditor {
 
 		self::check_if_ie();
 
-		Padma::load('visual-editor/display', 'VisualEditorDisplay');
+		Padma::load( 'visual-editor/display', 'VisualEditorDisplay' );
 		PadmaVisualEditorDisplay::display();
 
 	}
@@ -710,16 +751,17 @@ class PadmaVisualEditor {
 	public static function check_if_ie() {
 
 		/* Only show this on IE versions less than 9 */
-		if ( !padma_is_ie() || (padma_is_ie(9) || padma_is_ie(10) || padma_is_ie(11)) )
+		if ( !padma_is_ie() || (padma_is_ie(9) || padma_is_ie(10) || padma_is_ie(11)) ) {
 			return false;
+		}
 
 		$message = '<span style="text-align: center;font-size: 26px;width: 100%;display: block;margin-bottom: 20px;">Error</span>';
 
-		$message .= __('Unfortunately, the Padma Visual Editor does not work with Internet Explorer due to its lack of modern features.','padma') . '<br /><br />';
+		$message .= __( 'Unfortunately, the Padma Visual Editor does not work with Internet Explorer due to its lack of modern features.','padma' ) . '<br /><br />';
 
-		$message .= __('Please upgrade to a modern browser such as <a href="http://www.google.com/chrome" target="_blank">Google Chrome</a> or <a href="http://firefox.com" target="_blank">Mozilla Firefox</a>.','padma') . '<br /><br />'; 
+		$message .= __( 'Please upgrade to a modern browser such as <a href="http://www.google.com/chrome" target="_blank">Google Chrome</a> or <a href="http://firefox.com" target="_blank">Mozilla Firefox</a>.','padma' ) . '<br /><br />'; 
 
-		$message .= __('If this message persists after upgrading to a modern browser, please visit our <a href="https://www.padmaunlimited.com/community/" target="_blank">Community</a>.','padma');
+		$message .= __( 'If this message persists after upgrading to a modern browser, please visit our <a href="https://www.padmaunlimited.com/community/" target="_blank">Community</a>.','padma' );
 
 		return wp_die($message);
 
@@ -728,18 +770,18 @@ class PadmaVisualEditor {
 
 	public static function get_modes() {
 
-		return apply_filters('padma_visual_editor_get_modes', self::$modes);
+		return apply_filters( 'padma_visual_editor_get_modes', self::$modes);
 
 	}	
 
 
 	public static function get_current_mode() {
 
-		$mode = padma_get('visual-editor-mode');
+		$mode = padma_get( 'visual-editor-mode' );
 
 		if ( $mode ) {
 
-			if ( array_search(strtolower($mode), array_map('strtolower', array_keys(self::$modes))) ) {				
+			if ( array_search(strtolower($mode), array_map( 'strtolower', array_keys(self::$modes))) ) {				
 				return strtolower($mode);
 
 			} 
@@ -756,7 +798,7 @@ class PadmaVisualEditor {
 		if ( self::get_current_mode() === strtolower($mode) )
 			return true;
 
-		if ( !padma_get('visual-editor-mode') && strtolower($mode) === strtolower(self::$default_mode) )
+		if ( !padma_get( 'visual-editor-mode' ) && strtolower($mode) === strtolower(self::$default_mode) )
 			return true;
 
 		return false;
